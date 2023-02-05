74b6e4b3389c177faf5bd8662aab40664018c0a0
Justin Chung <jdchung@scu.edu>
2023-02-05 12:18:43 -0800

    Complete header

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import Header from "./components/header/header";
+import Home from "./components/home/home";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Home />
+    </>
+  );
+}
+
+export default App;
