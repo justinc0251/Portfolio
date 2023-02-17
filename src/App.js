ea550f5314f2915bf0570ee22d6a2bb498a0fa6e
Justin Chung <jdchung@scu.edu>
2023-02-17 02:10:37 -0800

    Finish contact section

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Header from "./components/header/header";
 import Home from "./components/home/home";
 import Skills from "./components/skills/skills"
+import Contact from "./components/contact/Contact";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <main className="main">
         <Home />
         <Skills />
+        <Contact />
       </main>
     </>
   );
