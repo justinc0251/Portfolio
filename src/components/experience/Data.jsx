6866748824225dfe8d2f8f6fd0563fe1d3540d0d
Justin Chung <jdchung@scu.edu>
2024-06-22 16:33:16 -0700

    add experience data

---
+++ src/components/experience/Data.jsx
@@ -0,0 +1,51 @@
+import Work1 from "../../assets/logos/scu.png";
+import Work2 from "../../assets/logos/vili.jpg";
+import Work3 from "../../assets/logos/goodday.jpg";
+
+export const accordionData = [
+  {
+    title: "Vili LLC, Software Engineer Intern",
+    date: "January 2024 - May 2024",
+    location: "Remote",
+    image: Work2,
+    content: [
+      "Developed frontend components for authentication and profile pages using React Native and TypeScript.",
+      "Enhanced client-server communication by designing RESTful APIs with Python and FastAPI.",
+      "Improved user sign-up rate by 30% by integrating Google, Apple, and WeChat authentication.",
+      "Maintained system stability and scalability by managing existing deployments using Docker.",
+    ],
+  },
+  {
+    title: "Santa Clara University, Research Assistant",
+    date: "September 2023 - June 2024",
+    location: "Santa Clara, CA",
+    image: Work1,
+    content: [
+      "Programmed a web scraping application with Python, Flask, BeautifulSoup, and Trafilatura.",
+      "Implemented natural language processing models (T5, BART, Pegasus, TextRank) for text summarization.",
+      "Evaluated the models by implementing text analysis techniques using LDA for keyword extraction and ROUGE metric for evaluation.",
+      "Increased visual appeal and engagement by 25% by utilizing the Stable Diffusion model to create customized images with text overlays for summarized articles.",
+    ],
+  },
+  {
+    title: "GoodDay, Software Engineer Intern",
+    date: "March 2023 - June 2023",
+    location: "Santa Clara, CA",
+    image: Work3,
+    content: [
+      "Collaborated on features for a mental health and addiction recovery app for iOS and Android devices.",
+      "Designed several main components and screens in React Native and JavaScript and developed the navigation between these screens.",
+      "Enhanced data management and enabled user interaction by integrating Firebase for backend services.",
+    ],
+  },
+  {
+    title: "Santa Clara University, Teacher's Assistant",
+    date: "September 2022 - Present",
+    location: "Santa Clara, CA",
+    image: Work1,
+    content: [
+      "Collaborating with a team of three students to grade assignments for an Intro to Engineering course.",
+      "Grading 100+ assignments per week while managing workload and meeting deadlines set by the professor.",
+    ],
+  },
+];
